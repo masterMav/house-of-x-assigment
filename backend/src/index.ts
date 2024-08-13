7ffb816f7359120e69ff12108abeb2f6df8ac57e
masterMav <mavdoesdev@gmail.com>
2024-08-13 18:03:14 +0530

    removed cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import appRouter from "./routes/routes";
-import cors from "cors";
 import { _init } from "./database";
 dotenv.config();
 
 _init();
 const app: Express = express();
 const port = process.env.PORT || 5000;
-app.use(cors<Request>());
 app.use(express.json());
 
 app.use("/", appRouter);
